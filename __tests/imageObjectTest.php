c188b61408ff167e8d9d93e48d87bfb701bf4f5c
Anael Mobilia <contrib@anael.eu>
2015-04-03 18:18:26 +0200

    test phpunit

---
+++ __tests/imageObjectTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of imageObjectTest
+ *
+ * @author Anael
+ */
+class imageObjectTest extends PHPUnit_Framework_TestCase {
+
+	    public function testPushAndPop()
+    {
+        $stack = array();
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+	
+}
