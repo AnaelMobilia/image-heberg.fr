29aee6d48256c206a33f5d25a672abecfc096662
Anael Mobilia <contrib@anael.eu>
2016-08-30 10:19:06 +0200

    Fix copyright...

--- membre/mesImages.php
+++ membre/mesImages.php
@@ -2,20 +2,20 @@
 /*
 * Copyright 2008-2015 Anael Mobilia
 *
-* This file is part of NextINpact-Unofficial.
+* This file is part of image-heberg.fr.
 *
-* NextINpact-Unofficial is free software: you can redistribute it and/or modify
+* image-heberg.fr is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 *
-* NextINpact-Unofficial is distributed in the hope that it will be useful,
+* image-heberg.fr is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 * GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with NextINpact-Unofficial. If not, see <http://www.gnu.org/licenses/>
+* along with image-heberg.fr. If not, see <http://www.gnu.org/licenses/>
 */
 // TODO : affichage des images dans la page en javascript ?
 // TODO : navigation entre les images
