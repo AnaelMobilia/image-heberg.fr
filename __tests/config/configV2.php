70f7fc11f0935ec469278db43c9efc3a7ed4daae
Anael Mobilia <contrib@anael.eu>
2016-10-26 01:48:47 +0200

    Include -> require pour lever une exception si requis

--- __tests/config/configV2.php
+++ __tests/config/configV2.php
@@ -85,8 +85,6 @@ spl_autoload_register(function ($class) {
     // Code spécifique Travis : pas de chargement des classes de PHPUnit
     if (strpos($class, "PHPUnit")) {
         require _PATH_ . 'classes/' . $class . '.class.php';
-    } else {
-        new Exception("Refus autoload " . $class);
     }
 });
 
