33d4df0b8803a52a8c833421f3664ee36f043a67
Anael Mobilia <contrib@anael.eu>
2016-10-26 00:39:17 +0200

    Config BDD

--- __tests/imageObjectTest.php
+++ __tests/imageObjectTest.php
@@ -1,16 +1,44 @@
 <?php
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2008-2016 Anael Mobilia
+ *
+ * This file is part of image-heberg.fr.
+ *
+ * image-heberg.fr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * image-heberg.fr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with image-heberg.fr. If not, see <http://www.gnu.org/licenses/>
  */
 
-/**
- * Description of imageObjectTest
- *
- * @author Anael
- */
 class imageObjectTest extends PHPUnit_Framework_TestCase {
+    private $maBDD;
+
+    function __construct() {
+        global $maBDD;
+        // BDD
+        define('_BDD_HOST_', 'localhost');
+        define('_BDD_USER_', 'root');
+        define('_BDD_PASS_', '');
+        define('_BDD_NAME_', 'imageheberg');
+
+        // Fonction de chargement des classes en cas de besoin
+        spl_autoload_register(function ($class) {
+            include _PATH_ . 'classes/' . $class . '.class.php';
+        });
+
+        // Connexion centralisée à la BDD
+        $maBDD = new PDO('mysql:host=' . _BDD_HOST_ . ';dbname=' . _BDD_NAME_, _BDD_USER_, _BDD_PASS_);
+        $maBDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $maBDD->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    }
 
     public function testPushAndPop() {
         $stack = array();
