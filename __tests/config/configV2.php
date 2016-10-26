b7cf5322305d5549dfba75b7f6d1d33ba345b8b8
Anael Mobilia <contrib@anael.eu>
2016-10-26 02:27:59 +0200

    debug

--- __tests/config/configV2.php
+++ __tests/config/configV2.php
@@ -84,7 +84,6 @@ define('_HEBERGEUR_SITE_', 'http://www.ovh.com');
 spl_autoload_register(function ($class) {
     // Code spécifique Travis : pas de chargement des classes de PHPUnit
     if (strpos($class, "PHPUnit") === FALSE && strpos($class, "Composer") === FALSE) {
-        throw new Exception(var_dump($class));
         require _PATH_ . 'classes/' . $class . '.class.php';
     }
 });
