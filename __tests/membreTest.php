88819c64197f21f6e57e47cfb092ee1fd78defe6
Anael Mobilia <contrib@anael.eu>
2016-10-31 21:38:47 +0100

    Fix test

--- __tests/membreTest.php
+++ __tests/membreTest.php
@@ -61,7 +61,7 @@ class membreTest extends PHPUnit_Extensions_Database_TestCase {
         /**
          * Récupération d'un objet
          */
-        $monMembre = new utilisateurObject(2);
+        $monMembre = new utilisateurObject();
 
         /**
          * Vérification des valeurs
