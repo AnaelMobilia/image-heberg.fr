aab0f09e53fccd8f212f8bdd8124513a64348ad2
Anael Mobilia <contrib@anael.eu>
2016-11-05 00:55:50 +0100

    Meilleure détection des non sessions

--- upload.php
+++ upload.php
@@ -145,10 +145,8 @@ if (!$erreur) {
  */
 if (!$erreur) {
     $maSession = new sessionObject();
-    echo $maSession->getId();
-    var_dump($maSession->getId());
     // Si j'ai un ID d'utilisateur en session && que cette image n'est pas déjà enregistrée
-    if ($maSession->getId() !== 0 && is_null($doublon)) {
+    if (is_int($maSession->getId()) && $maSession->getId() !== 0 && is_null($doublon)) {
         // Nouvelle image : assignation à l'utilisateur
         $monUtilisateur = new utilisateurObject($maSession->getId());
         $monUtilisateur->assignerImage($monImage);
