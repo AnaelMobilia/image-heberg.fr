4875a2ad3e5d4c98edf6e21c4a4064d86eae22cc
Anael Mobilia <contrib@anael.eu>
2016-11-05 04:09:56 +0100

    bootstrap

--- index.php
+++ index.php
@@ -28,66 +28,68 @@ $_SESSION['_upload'] = TRUE;
 <div class="jumbotron">
     <h1><small>Envoyer une image</small></h1>
 
+    <div class = "alert alert-info">
+        Image-heberg.fr est un service gratuit vous permettant d'héberger vos images sur internet.
+        <ul>
+            <li>Image de type JPG, PNG, GIF</li>
+            <li>Taille maximale : <?= round(_IMAGE_POIDS_MAX_ / 1048576, 1) ?> Mo</li>
+            <li>Dimensions maximales : <?= _IMAGE_DIMENSION_MAX_ ?> x <?= _IMAGE_DIMENSION_MAX_ ?> pixels (hauteur x largeur)</li>
+        </ul>
+    </div>
+
     <div class="panel panel-primary">
         <div class="panel-body">
-            Image-heberg.fr est un service gratuit vous permettant d'héberger vos images sur internet.
-            <ul>
-                <li>Images de type JPG, PNG, GIF</li>
-                <li>Taille max. : <?= round(_IMAGE_POIDS_MAX_ / 1048576, 1) ?> Mo</li>
-                <li>Dimensions max. : <?= _IMAGE_DIMENSION_MAX_ ?> x <?= _IMAGE_DIMENSION_MAX_ ?> pixels (hauteur x largeur)</li>
-            </ul>
-            <form enctype="multipart/form-data" action="upload.php" method="post" class="form-inline">
-                <div class="form-group">
-                    <fieldset>
-                        <legend>Envoyer un fichier :</legend>
-                        <input name="fichier" type="file" />
-                        <br />
-                        <button class="btn btn-info" name="Submit" type="submit"><strong>Envoyer</strong></button>
-                        <br />
-                        <em>Tout envoi de fichier implique l'acceptation des <a href="/cgu.php">Conditions Générales d'Utilisation</a> du service.</em>
-                    </fieldset>
+            <form enctype="multipart/form-data" action="upload.php" method="post" class="form-group">
+                <div class="form-group form-group-lg">
+                    <label for="fichier" class="col-sm-3">Fichier à envoyer</label>
+                    <div class="col-sm-9">
+                        <input type="file" name="fichier" id="fichier">
+                        <span class="help-block">Tout envoi de fichier implique l'acceptation des <a href="/cgu.php">Conditions Générales d'Utilisation</a> du service.</span>
+                    </div>
                 </div>
-                <br />
                 <div class="form-group">
-                    <fieldset>
-                        <legend>
-                            <span id="options">
-                                Options : <!--<img id="image_options" src="/template/images/fleche_bas.png" alt="Options" style="height: 15px; width: 15px;"/>-->
-                            </span>
-                        </legend>
-                        <div id="liste_options">
-    <!--                            <input disabled type="checkbox" name="thumbs" />Faire une miniature
-                            <select disabled name="t_size">
-                                <option value="88x31">Bouton (88x31)</option>
-                                <option value="90x90">Avatar (90x90)</option>
-                                <option value="100x100">Aper&ccedil;u (100x100)</option>
-                                <option value="140x100">Miniature (140x100)</option>
-                                <option value="320x240">Miniature+ (320x240)</option>
-                                <option value="640x480" selected="selected">Miniature++ (640x480)</option>
-                                <option value="700x700">Miniature Forum (700x700)</option>
-                            </select>
-                            <br />
-                            <input type="checkbox" name="t_info" disabled="disabled" />Information poids / taille original
-                            <br />
-                            <input disabled="disabled" type="checkbox" name="resize" />Redimensionner l'image
-                            <select disabled="disabled" name="size">
-                                <option value="320x240">320x240</option>
-                                <option value="640x480">640x480</option>
-                                <option value="800x600">800x600</option>
-                                <option value="1024x768">1024x768</option>
-                            </select>
-                            <br />-->
-                            <input type="checkbox" name="rotate"/>&nbsp;Rotation de l'image
-                            <select name="angleRotation">
-                                <optgroup label="Rotation vers la gauche">
-                                    <option value="90">90&deg; (&frac14; tour)</option>
-                                    <option value="180">180&deg; (&frac12; tour)</option>
-                                    <option value="270">270&deg; (&frac34; tour)</option>
-                                </optgroup>
-                            </select>
-                        </div>
-                    </fieldset>
+                    <label class="col-sm-3">Rotation de l'image</label>
+                    <div class="col-sm-9">
+                        <select name="angleRotation" class="form-control">
+                            <option value="" selected>-- Ne pas effectuer --</option>
+                            <optgroup label="Rotation vers la gauche">
+                                <option value="90">90&deg; (&frac14; tour)</option>
+                                <option value="180">180&deg; (&frac12; tour)</option>
+                                <option value="270">270&deg; (&frac34; tour)</option>
+                            </optgroup>
+                        </select>
+                    </div>
                 </div>
+                <div class="form-group">
+                    <label class="col-sm-3">Faire une miniature</label>
+                    <div class="col-sm-9">
+                        <select name="dimMiniature" class="form-control" disabled>
+                            <option value="" selected>-- Option non disponible actuellement --</option>
+                            <option value="">-- Ne pas effectuer --</option>
+                            <option value="700x700">Miniature Forum (700x700)</option>
+                            <option value="640x480">Miniature++ (640x480)</option>
+                            <option value="320x240">Miniature+ (320x240)</option>
+                            <option value="140x100">Miniature (140x100)</option>
+                            <option value="100x100">Aperçu (100x100)</option>
+                            <option value="90x90">Avatar (90x90)</option>
+                            <option value="88x31">Bouton (88x31)</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-3">Redimensionner l'image</label>
+                    <div class="col-sm-9">
+                        <select name="dimImage" class="form-control" disabled>
+                            <option value="" selected>-- Option non disponible actuellement --</option>
+                            <option value="">-- Ne pas effectuer --</option>
+                            <option value="320x240">320x240</option>
+                            <option value="640x480">640x480</option>
+                            <option value="800x600">800x600</option>
+                            <option value="1024x768">1024x768</option>
+                        </select>
+                    </div>
+                </div>
+                <button class="btn btn-info" name="Submit" type="submit">Envoyer</button>
             </form>
         </div>
     </div>
