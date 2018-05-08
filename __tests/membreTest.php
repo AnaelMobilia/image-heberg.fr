5c8b9c51cfb0e5b343fe6e99389210f223cac372
Anael Mobilia <contrib@anael.eu>
2018-05-08 20:17:30 +0200

    debug phpunit

--- __tests/membreTest.php
+++ __tests/membreTest.php
@@ -188,9 +188,9 @@ class membreTest extends TestCase {
       /**
        *  Appel de la page
        */
-      //ob_start();
+      ob_start();
       require 'membre/monCompte.php';
-      //ob_end_clean();
+      ob_end_clean();
 
       /**
        * Récupération d'un objet
@@ -232,6 +232,7 @@ class membreTest extends TestCase {
        * Vérification des valeurs
        */
       $this->assertEquals(FALSE, $monMembre->connexion('username', $_POST['userPasswordDelete']), "connexion ne devrait plus être possible");
+      $this->assertEquals(FALSE, $monMembre->connexion('username', 'password'), "connexion ne devrait plus être possible");
    }
 
    /**
