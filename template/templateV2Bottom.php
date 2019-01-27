c8d5509aa49c55c896cab48de4ecb38fdf9fda65
Anael MOBILIA <contrib@anael.eu>
2019-01-27 11:55:51 +0100

    update jquery

--- template/templateV2Bottom.php
+++ template/templateV2Bottom.php
@@ -47,7 +47,7 @@
         </p>
     </div>
 </footer>
-<script src="template/js/jquery-3.2.1.slim.min.js"></script>
+<script src="template/js/jquery-3.3.1.slim.min.js"></script>
 <script src="template/js/bootstrap-3.3.7.min.js"></script>
 <script src="template/js/js.php"></script>
 </body>
