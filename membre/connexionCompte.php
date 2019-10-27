8f4c245057ee1af194fcd416ea5facc34bba4d8f
Anael MOBILIA <contrib@anael.eu>
2019-10-27 22:12:47 +0100

    Amélioration du rendu visuel

--- membre/connexionCompte.php
+++ membre/connexionCompte.php
@@ -18,7 +18,7 @@
  * along with image-heberg.fr. If not, see <http://www.gnu.org/licenses/>
  */
 if (!defined('_TRAVIS_')) {
-   require __DIR__ . '/../config/config.php';
+    require __DIR__ . '/../config/config.php';
 }
 
 // Un utilisateur...
@@ -26,36 +26,36 @@ $monUtilisateur = new utilisateurObject();
 
 // En cas de validation du formulaire
 if (isset($_POST['valider'])) {
-   $messageErreur = '';
+    $messageErreur = '';
 
-   if (empty($_POST['userName'])) {
-      $messageErreur .= "<br />Merci de saisir un identifiant.";
-   }
-   if (empty($_POST['userPassword'])) {
-      $messageErreur .= "<br />Merci de saisir un mot de passe.";
-   }
-   // Si tout est bon
-   if (empty($messageErreur)) {
-      if ($monUtilisateur->connexion($_POST['userName'], $_POST['userPassword']) === TRUE) {
-         if (!_TRAVIS_) {         
-            // Succès -> redirige sur la page d'accueil
-            header('Location: ' . _URL_);
-            die();
-         }
-      } else {
-         $messageErreur .= "<br />Erreur dans vos identifiants.";
-      }
-   }
+    if (empty($_POST['userName'])) {
+        $messageErreur .= "<br />Merci de saisir un identifiant.";
+    }
+    if (empty($_POST['userPassword'])) {
+        $messageErreur .= "<br />Merci de saisir un mot de passe.";
+    }
+    // Si tout est bon
+    if (empty($messageErreur)) {
+        if ($monUtilisateur->connexion($_POST['userName'], $_POST['userPassword']) === TRUE) {
+            if (!_TRAVIS_) {
+                // Succès -> redirige sur la page d'accueil
+                header('Location: ' . _URL_);
+                die();
+            }
+        } else {
+            $messageErreur .= "<br />Erreur dans vos identifiants.";
+        }
+    }
 }
 
 require _TPL_TOP_;
 // Affichage des erreurs si requis
 if (isset($messageErreur)) :
-   ?>
-   <div class="alert alert-danger">
-       <strong>La connexion à votre compte n'est pas possible :</strong>
-       <?= $messageErreur ?>
-   </div>
+    ?>
+    <div class="alert alert-danger">
+        <strong>La connexion à votre compte n'est pas possible :</strong>
+        <?= $messageErreur ?>
+    </div>
 <?php endif; ?>
 <h1><small>Se connecter à mon compte</small></h1>
 
@@ -68,14 +68,8 @@ if (isset($messageErreur)) :
         <label for="userPassword">Mot de passe</label>
         <input type="password" class="form-control" name="userPassword" id="userPassword" placeholder="Mot de passe" required="required">
     </div>
-    <div class="form-group">
-        <!-- // TODO -->
-        <a href="#">Mot de passe oublié (à venir)</a>
-    </div>
     <button type="submit" name="valider" class="btn btn-success">Se connecter</button>
-</form>
-<br />
-<form action="<?= _URL_MEMBRE_ ?>creerCompte.php">
-    <button type="submit" class="btn btn-info">Créer un compte</button>
+    <a class="btn btn-warning" href="#" role="button">Mot de passe oublié (à venir)</a>
+    <a class="btn btn-info" href="<?= _URL_MEMBRE_ ?>creerCompte.php" role="button">Créer un compte</a>
 </form>
 <?php require _TPL_BOTTOM_ ?>
