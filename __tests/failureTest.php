b54d70cd3580d76c0893bbec24ed6f9c7cd0d72a
Anael Mobilia <AnaelMobilia@users.noreply.github.com>
2020-04-03 17:44:41 +0200

    Test de travis

---
+++ __tests/failureTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * Copyright 2008-2020 Anael MOBILIA
+ *
+ * This file is part of image-heberg.fr.
+ *
+ * image-heberg.fr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * image-heberg.fr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with image-heberg.fr. If not, see <http://www.gnu.org/licenses/>
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class failureTest extends TestCase {
+
+    public function testTravis() {
+        // Test le fonctionnement de Travis...
+        $this->assertEquals(1, 0);
+    }
+
+}
