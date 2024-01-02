4821839b5c6a23dbe3325fa5a55673966439ea53
Anael MOBILIA <contrib@anael.eu>
2024-01-02 21:54:43 +0100

    Ajout de détails dans les CGU sur les images non autorisées

--- cgu.php
+++ cgu.php
@@ -35,8 +35,21 @@ require _TPL_TOP_;
         <div class="card-body">
             <ul>
                 <li>Toutes images de type JPG, PNG, GIF.</li>
-                <li>Contenu conforme à la législation française.</li>
-                <li>Pornographie et érotisme non autorisés.</li>
+                <li>Contenus conformes à la législation française.</li>
+                <li>L'administrateur du site est seul juge de la conformité d'une image aux présentes CGU. En cas de désaccord, vous pouvez le contacter via <a href="/contact.php">le formulaire de contact</a>.</li>
+            </ul>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            Contenus non autorisés
+        </div>
+        <div class="card-body">
+            <ul>
+                <li>Contenus non conformes à la législation française.</li>
+                <li>Pornographie et érotisme.</li>
+                <li>Scènes de violence (cadavre, torture, maltraitance, image dégradante, ...) ou d'appel à la violence.</li>
+                <li>Pour information, les images peuvent être analysées par un composant d'IA dédié au service afin d'aider à leur vérification.</li>
             </ul>
         </div>
     </div>
@@ -47,7 +60,7 @@ require _TPL_TOP_;
         <div class="card-body">
             <ul>
                 <li>Gratuit.</li>
-                <li>Traffic illimité <em>(hors abus)</em>.</li>
+                <li>Traffic illimité <em>(hors abus, auquel cas des limitations peuvent être mises en place)</em>.</li>
                 <li>
                     <b>Conservation :</b>
                     <ul>
@@ -66,11 +79,10 @@ require _TPL_TOP_;
                     Les fichiers restent votre propriété.
                 </li>
                 <li>
-                    Aucune suppression sur demande d'un utilisateur.
-                    <em>(utilisez la fonction de suppression à l'envoi ou utilisez <a href="membre/connexionCompte.php">l'espace membre)</a></em>
+                    Aucune suppression sur demande d'un utilisateur. <em>(utilisez la fonction de suppression à l'envoi ou utilisez <a href="membre/connexionCompte.php">l'espace membre)</a></em>
                 </li>
                 <li>
-                    Toutes les données possédées seront fournies en cas de demande judiciaire.
+                    Toutes les données possédées seront fournies en cas de demande judiciaire ou de dépôt de plainte.
                 </li>
                 <li>
                     Suppression des comptes membres qui n'ont jamais été utilisés au bout de <?= _DELAI_EFFACEMENT_COMPTES_JAMAIS_UTILISES_ ?> jour<?= _DELAI_EFFACEMENT_COMPTES_JAMAIS_UTILISES_ > 1 ? 's' : '' ?>.
@@ -80,7 +92,7 @@ require _TPL_TOP_;
                 </li>
             </ul>
             <div class="card-footer">
-                <em>Mises à jour le 18 décembre 2022 : ajout de la suppression des comptes membres jamais utilisés</em>
+                <em>Mises à jour le 02 janvier 2024 : ajout de précisions sur les images non autorisées + module de catégorisation IA.</em>
             </div>
         </div>
     </div>
