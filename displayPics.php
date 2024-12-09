4294c9bfa3d4f404aff69016df9302ac67b5c20e
Anael MOBILIA <contrib@anael.eu>
2024-12-09 22:22:05 +0100

    #45 - Ne pas bloquer l'image 404 si l'image demandée n'existe plus

--- displayPics.php
+++ displayPics.php
@@ -69,12 +69,10 @@ if (
     $monObjet->charger(_IMAGE_404_);
     // Envoi d'un header en 404
     header('HTTP/2 404 Not Found');
-}
-
-/**
+} /**
  * Détection des User-Agent malveillant et blocage des images demandées
  */
-if (
+elseif (
     isset($_SERVER['HTTP_USER_AGENT'])
     && in_array($_SERVER['HTTP_USER_AGENT'], _ABUSE_DISABLE_PICS_WHEN_USERE_AGENT_, true)
 ) {
